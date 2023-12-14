b9b1c0d6fadf172b6ac5466868d9c59fb8a52979
Dark <cajusneumann@gmail.com>
2023-12-14 11:24:33 +0100

    renamed

--- commands/oldclear.py
+++ commands/oldclear.py
@@ -9,16 +9,16 @@ path.append(str(path_root))
 
 from utils.OwnerCheck import Owner
 
-class clear(Cog):
+class oldclear(Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
     
-    @slash_command(description="Clears MSGs")
-    async def clear(self, interaction: ApplicationCommandInteraction, number:int):
+    @slash_command(description="oldclears MSGs")
+    async def oldclear(self, interaction: ApplicationCommandInteraction, number:int):
         if not await Owner().isOwner(self.bot, interaction.user.id):
             await interaction.response.send_message("You are not a Owner!\nYou are not Permitted to use this command")
             return
-        wip_embed = Embed(title="Clear Command", description="Im working on it!\nMay take a while!", color=Color.orange())
+        wip_embed = Embed(title="oldclear Command", description="Im working on it!\nMay take a while!", color=Color.orange())
         await interaction.response.send_message(embed=wip_embed,ephemeral=True)
         mgs = [] #Empty list to put all the messages in the log
         #Converting the amount of messages to delete to an integer
@@ -26,10 +26,10 @@ class clear(Cog):
             mgs.append(x)
         for x in mgs:
             await x.delete()
-        done_embed = Embed(title="Clear Command", description="Everything is done now!", color=Color.green())
+        done_embed = Embed(title="oldclear Command", description="Everything is done now!", color=Color.green())
         await interaction.edit_original_response(embed=done_embed)
         
 
 
 def setup(bot: Bot) -> None:
-    bot.add_cog(clear(bot))
+    bot.add_cog(oldclear(bot))
